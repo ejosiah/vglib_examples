6104e92502394942d7879e44b04fc837d88cd61e
Josiah Ebhomenye <joebhomenye@gmail.com>
2024-04-09 23:25:12 +0100

    use spatial hash acceleration structure

--- examples/sph2d/shaders/hash.glsl
+++ examples/sph2d/shaders/hash.glsl
@@ -1,18 +1,26 @@
 #ifndef HASH_GLSL
 #define HASH_GLSL
 
-int hash(ivec2 pid) {
-    return (pid.x * 92837111) ^ (pid.y * 689287499);
-}
+//int hash(ivec2 pid) {
+//    return (pid.x * 92837111) ^ (pid.y * 689287499);
+//}
 
 //int computeHash(vec2 pos) {
 //    ivec2 pid = ivec2(pos/spacing);
 //    return abs(hash(pid)) % hashMapSize;
 //}
 
-int computeHash(vec2 pos) {
-    ivec2 pid = ivec2(pos/spacing);
+ivec2 intCoords(vec2 pos) {
+    return ivec2(pos/spacing);
+}
+
+int hash(ivec2 pid) {
     return pid.y * int(20/spacing) + pid.x;
+}
+
+int computeHash(vec2 pos) {
+    ivec2 pid = intCoords(pos);
+    return hash(pid);
 }
 
 #endif // HASH_GLSL
